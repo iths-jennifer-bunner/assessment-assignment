a79fdf01cd9e7f2504e0cd60edf832362baa1222
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-28 20:17:41 +0200

    debug

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-const PORT = process.env.PORT || 3006;
 
 export default axios.create({
-  baseURL: "https://my-little-message-app.herokuapp.com:" + PORT,
+  baseURL: "https://my-little-message-app.herokuapp.com/",
 });
