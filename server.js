725d94a8ea81834b395a670c5798eb021c4fe4f1
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-28 13:25:52 +0200

    troubleshootin heroku

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ const router = jsonServer.router("./server-api/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
+
 const PORT = process.env.PORT || 4000;
 server.use(middlewares);
-// server.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
 server.use(router);
+
 server.listen(PORT, () => {
   console.log("Server is running");
 });
