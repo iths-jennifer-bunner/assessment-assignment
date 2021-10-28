d68c321bd0c670beef8f7ec21988483322fddf58
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-28 21:20:10 +0200

    debug

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./server-api/db.json");
+const path = require("path");
+const express = require("express");
+
+const router = jsonServer.router("./db.json");
 
 const middlewares = jsonServer.defaults();
 
 const PORT = process.env.PORT || 3006;
 
 server.use(middlewares);
-// server.use(
-//   jsonServer.rewriter({
-//     "/api/*": "/$1",
-//   })
-// );
+server.use(express.static(path.join(__dirname, "build")));
+
 server.use(router);
+server.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/../build/index.html"));
+});
+
 server.listen(PORT, () => {
   console.log("Server is running");
 });
