73d17814e680eb302c87ead44d4e1d09c06ea0f6
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-28 13:32:22 +0200

    troubleshooting heroku depl

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const router = jsonServer.router("./server-api/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
-
 const PORT = process.env.PORT || 4000;
 server.use(middlewares);
-server.use(router);
 
+server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
 });
