5cfdcfdbbf57e35e9c855939e315f2defd9eef82
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-29 21:41:18 +0200

    debug

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 // require("dotenv").config();
-const path = require("path");
-const express = require("express");
+// const path = require("path");
+// const express = require("express");
 const middlewares = jsonServer.defaults({ static: "./build" });
 const router = jsonServer.router("./db.json");
 const PORT = process.env.PORT || 3006;
@@ -10,13 +10,13 @@ const PORT = process.env.PORT || 3006;
 // router.connect({
 //   databaseURL: process.env.DATABASE_URL,
 // });
-server.use("/*", middlewares, router);
-server.use(express.static(path.join(__dirname, "./build")));
+// server.use("/*", middlewares, router);
+// server.use(express.static(path.join(__dirname, "./build")));
 
-server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build", "index.html"));
-});
-// server.use(middlewares);
+// server.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./build", "index.html"));
+// });
+server.use(middlewares);
 
 server.use(router);
 
