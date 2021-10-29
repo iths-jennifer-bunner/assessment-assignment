68a0a8fcbf05592367b49867235f87f88bdb12f8
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-29 20:43:21 +0200

    changed baseUrl

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://my-little-message-app.herokuapp.com",
+  baseURL: "http://localhost:3006",
 });
 // databaseURL: "https://my-little-message-app.herokuapp.com/",
