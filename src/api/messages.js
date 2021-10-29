9165a5ec6aa67104824d07f5e5949d2b9169c2fb
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-29 21:00:42 +0200

    debug

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3006",
+  baseURL: "https://my-little-message-app.herokuapp.com",
 });
 // databaseURL: "https://my-little-message-app.herokuapp.com/",
