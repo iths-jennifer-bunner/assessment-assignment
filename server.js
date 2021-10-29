17854b35c1d8160b0568a23ceb5c9b2533db7c1b
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-29 09:25:31 +0200

    debug

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const path = require("path");
-const express = require("express");
+// const express = require("express");
 const router = jsonServer.router("./db.json");
 
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3006;
 
 server.use(middlewares);
-server.use(express.static(path.join(__dirname, "build")));
+// server.use(express.static(path.join(__dirname, "build")));
 
 server.use(router);
-server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../build/index.html"));
-});
+// server.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/../build/index.html"));
+// });
 
 server.listen(PORT, () => {
-  console.log("Server is running", PORT);
+  console.log("Server is running:", PORT);
 });
