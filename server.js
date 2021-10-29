b37a32f9f3b590fe5898f3d47046057b3be55ad4
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2021-10-29 18:32:09 +0200

    debug

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use("./db.json", middlewares, router);
 server.use(express.static(path.join(__dirname, "build")));
 
 server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../build", "index.html"));
+  res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 // server.use(middlewares);
 
