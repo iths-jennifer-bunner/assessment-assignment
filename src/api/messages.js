f2b4cf74bcd0751c85f1101dede5498348044d0b
iths-jennifer-bunner <jenniferbunner@hotmail.com>
2022-03-31 17:03:45 +0200

    change baseURL

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://my-little-message-app.herokuapp.com/api",
+  baseURL: 'https://my-little-message-app.herokuapp.com',
 });
